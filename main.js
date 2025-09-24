2c9efd9029cc0c07052d8061080eb2cd635d7f41
khansaddamnr <saddamkhannikhat09911@gmail.com>
2025-09-24 11:37:34 +0530

    initial-commit

---
+++ main.js
@@ -0,0 +1,58 @@
+//active
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("scroll-on");
+    }else{
+        nav.classList.remove("scroll-on");
+    }
+}
+
+
+//nav
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
+
+//counter design
+document.addEventListener("DOMContentLoaded", () =>{
+function counter(id, start, end, duration){
+    let obj = document.getElementById(id),
+    current = start,
+    range = end - start,
+    increment = end > start ? 1 : -1,
+    step = Math.abs(Math.floor(duration / range)),
+    timer = setInterval(() =>{
+        current += increment;
+        obj.textContent = current;
+        if(current == end){
+            clearInterval(timer);
+        }
+    }, step);
+}
+counter("count1", 0, 1278, 3000);
+counter("count2", 0, 5786, 3000);
+counter("count3", 0, 1440, 3000);
+counter("count4", 0, 7110, 3000);
+});
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
